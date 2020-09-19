5cb1b9ce92bb726a3c97db5b9c2940bf8e587e6a
Everett Griffiths <everett@fireproofsocks.com>
2020-09-19 10:38:13 -0400

    Bumps version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -1,12 +1,12 @@
 defmodule Tacocat.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
 
   def project do
     [
       app: :tacocat,
-      description: "Playful text manipulations including upside down and backwards",
+      description: "Playful text manipulations including upside down, backwards, and Bon Iver!",
       version: @version,
       package: package(),
       elixir: "~> 1.10",
